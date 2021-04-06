489038971b45c6106e5864e55f6ee1eb025f32f3
Chris L <stylesuxx@gmail.com>
2021-04-07 01:07:49 +0200

    Removed unused vars

--- src/Components/Home/__tests__/index.test.jsx
+++ src/Components/Home/__tests__/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  render, screen, act, fireEvent, createEvent,
+  render, screen,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
