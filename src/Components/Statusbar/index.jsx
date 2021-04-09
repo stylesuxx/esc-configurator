5f8f025ed590eaa0cc253460cfad3280635f2f22
Chris L <stylesuxx@gmail.com>
2021-04-10 01:03:22 +0200

    Refactored some components

--- src/Components/Statusbar/index.jsx
+++ src/Components/Statusbar/index.jsx
@@ -8,11 +8,34 @@ import React, {
 
 import './style.scss';
 
+function BatteryState({
+  danger,
+  text,
+}) {
+  const { t } = useTranslation('common');
+
+  if(text) {
+    return (
+      <span className={danger ? 'danger' : ''}>
+        {`${t('battery')} ${text}`}
+      </span>
+    );
+  }
+
+  return null;
+}
+BatteryState.defaultProps = { text: undefined };
+BatteryState.propTypes = {
+  danger: PropTypes.bool.isRequired,
+  text: PropTypes.string,
+};
+
 const Statusbar = forwardRef(({
   packetErrors,
   version,
 }, ref) => {
   const { t } = useTranslation('common');
+
   const cellLimit = 3.7;
   const [utilization, setUtilization] = useState({
     up: 0,
@@ -43,18 +66,6 @@ const Statusbar = forwardRef(({
     },
   }));
 
-  function BatteryState() {
-    if(batteryState.text) {
-      return (
-        <span className={batteryState.danger ? 'danger' : ''}>
-          {`${t('battery')} ${batteryState.text}`}
-        </span>
-      );
-    }
-
-    return null;
-  }
-
   return (
     <div id="status-bar">
       <span>
@@ -65,7 +76,10 @@ const Statusbar = forwardRef(({
         {`${t('statusbarPacketError')} ${packetErrors}`}
       </span>
 
-      <BatteryState />
+      <BatteryState
+        danger={batteryState.danger}
+        text={batteryState.text}
+      />
 
       <span className="version">
         {version}
@@ -73,7 +87,6 @@ const Statusbar = forwardRef(({
     </div>
   );
 });
-
 Statusbar.propTypes = {
   packetErrors: PropTypes.number.isRequired,
   version: PropTypes.string.isRequired,
