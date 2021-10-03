27a450967b6b61a870ae274cb778bd03734f9e1c
Chris L <stylesuxx@gmail.com>
2021-10-03 12:16:39 +0200

    Import alphabetically

--- src/sources/index.js
+++ src/sources/index.js
@@ -4,23 +4,23 @@ import {
   ARM_TYPES,
 } from './Source';
 
+import am32Source from './AM32';
 import blheliSource from './Blheli';
 import bluejaySource from './Bluejay';
-import am32Source from './AM32';
 
 const sources = [
+  am32Source,
   bluejaySource,
   blheliSource,
-  am32Source,
 ];
 
 export {
   PLATFORMS,
   SILABS_TYPES,
   ARM_TYPES,
+  am32Source,
   blheliSource,
   bluejaySource,
-  am32Source,
 };
 
 export default sources;
