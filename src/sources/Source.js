b4d904afd6977c691ff5ae6a5c6d43c04925bd48
Mathias Rasmussen <mathiasvr@gmail.com>
2022-01-17 17:24:28 +0100

    Fetch caching

--- src/sources/Source.js
+++ src/sources/Source.js
@@ -1,12 +1,9 @@
 import {
-  LocalDataNotAvailableError,
   MethodNotImplementedError,
   MissingParametersError,
 } from '../utils/Errors';
 
-import settings from '../settings.json';
-
-const { corsProxy } = settings;
+import { fetchJsonCached } from '../utils/Fetch';
 
 /* Abstract Base Class for firmware sources
  *
@@ -24,43 +21,14 @@ class Source {
     this.escs = escs;
     this.pwm = [];
 
-    this.fetchJson = async (url) => {
-      try {
-        const proxy = `${corsProxy}${url}`;
-        const response = await fetch(proxy);
-        if(!response.ok) {
-          throw new Error(response.statusText);
-        }
-
-        return response.json();
-      } catch(e) {
-        throw new Error(e);
-      }
-    };
-
-    this.getRemoteVersionsList = async (url) => {
-      const localStorageKey = `${this.getName()}_versions`;
-
-      try {
-        const result = await this.fetchJson(url);
-        localStorage.setItem(localStorageKey, JSON.stringify(result));
-
-        return result;
-      } catch(e) {
-        const content = localStorage.getItem(localStorageKey);
-
-        if(content !== null) {
-          return (JSON.parse(content));
-        }
-      }
-
-      throw new LocalDataNotAvailableError();
-    };
-
     this.setLocalVersions = async(versions) => {
       const localStorageKey = `${this.getName()}_versions`;
       localStorage.setItem(localStorageKey, JSON.stringify(versions));
     };
+  }
+
+  async getRemoteVersionsList(url) {
+    return await fetchJsonCached(url);
   }
 
   buildDisplayName() {
---
+++ src/utils/Fetch.js
@@ -0,0 +1,52 @@
+import { corsProxy } from '../settings.json';
+
+async function fetchProxy(url) {
+  return fetch(`${corsProxy}${url}`);
+}
+
+// Fetch content online
+async function fetchJson(url) {
+  const response = await fetchProxy(url);
+  if(!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}
+
+async function fetchAndCache(cache, url) {
+  const response = await fetchProxy(url);
+
+  const newHeaders = new window.Headers(response.headers);
+  newHeaders.set('Time-Cached', Date.now().toString());
+  const newResponse = new window.Response(response.body, { headers: newHeaders });
+  cache.put(url, newResponse.clone());
+
+  return newResponse;
+}
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+// Fetch content from cache or online if necessary
+async function fetchJsonCached(url, opts = { maxAge: ONE_DAY }) {
+  const cache = await window.caches.open('v1');
+  let cachedResponse = await cache.match(url);
+
+  const isOld = (r) => Date.now() - parseInt(r.headers.get('Time-Cached'), 10) > opts.maxAge;
+
+  if (!cachedResponse || !cachedResponse.ok) {
+    // Fetch response and cache it
+    cachedResponse = await fetchAndCache(cache, url);
+  } else if (isOld(cachedResponse)) {
+    // Fetch new version and cache it for next time,
+    // but return old response now to save time
+    fetchAndCache(cache, url);
+  }
+
+  return await cachedResponse.json();
+}
+
+export {
+  fetchJson,
+  fetchJsonCached,
+};
