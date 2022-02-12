4d5bca41ae5c1f060fb9b970022b0bc7cb26ec36
Mathias Rasmussen <mathiasvr@gmail.com>
2022-02-12 12:55:58 +0100

    Precache static json files and favicon.ico (#219)

--- src/service-worker.js
+++ src/service-worker.js
@@ -64,9 +64,25 @@ registerRoute(
  */
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => url.origin === self.location.origin && (url.pathname.endsWith('.png') || url.pathname === '/favicon.ico'),
+  // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
+    plugins: [
+
+      /*
+       * Ensure that once this runtime cache reaches a maximum size the
+       * least-recently used images are removed.
+       */
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.json'),
+  new StaleWhileRevalidate({
+    cacheName: 'json',
     plugins: [
 
       /*
