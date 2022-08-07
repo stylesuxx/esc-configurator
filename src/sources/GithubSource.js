548ce05486f0956fd74bc00b487f46939f4830dc
Chris L <stylesuxx@gmail.com>
2022-08-07 13:38:42 +0200

    Simplified filtering and transformation of available releases

--- src/sources/GithubSource.js
+++ src/sources/GithubSource.js
@@ -4,16 +4,19 @@ import { fetchJsonCached } from '../utils/Fetch';
 class GithubSource extends Source {
   async getRemoteVersionsList(repo, blacklist = []) {
     const githubReleases = await fetchJsonCached(`https://api.github.com/repos/${repo}/releases`);
+    const releasesWithAssets = githubReleases.filter(
+      (release) => release.assets.length && !blacklist.includes(release.tag_name)
+    );
 
-    return githubReleases
-      .filter((r) => r.assets.length && !blacklist.includes(r.tag_name)) // hide releases without assets
-      .map((r) => ({
-        name: r.name || r.tag_name.replace(/^v/, ''),
-        key: r.tag_name,
-        url: `https://github.com/${repo}/releases/download/${r.tag_name}/`,
-        prerelease: r.prerelease,
-        published_at: r.published_at,
-      }));
+    const validReleases = releasesWithAssets.map((release) => ({
+      name: release.name || release.tag_name.replace(/^v/, ''),
+      key: release.tag_name,
+      url: `https://github.com/${repo}/releases/download/${release.tag_name}/`,
+      prerelease: release.prerelease,
+      published_at: release.published_at,
+    }));
+
+    return validReleases;
   }
 }
 
