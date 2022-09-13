294568e5b0671d5d50433795dc16749df8a0df74
Chris L <stylesuxx@gmail.com>
2022-09-13 12:08:59 +0200

    Adjust page size and flash size. Flash size includes more than the actual flash so that the bootloader has space too in image generation - even if it is not written

--- src/utils/Hardware/Silabs.js
+++ src/utils/Hardware/Silabs.js
@@ -20,8 +20,8 @@ const mcus = {
   "E8B5": {
     "name": "EFM8BB51x",
     "signature": "0xE8B5",
-    "page_size": 512,
-    "flash_size": 8192,
+    "page_size": 2048,
+    "flash_size": 63485,
     "bootloader_address": "0xF000",
     "eeprom_offset": "0x3000",
   },
