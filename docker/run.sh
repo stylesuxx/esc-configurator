8f4e390857da7643da6491e64368b716ab6b9bfe
Johnny Halfmoon <jhalfmoon@milksnot.com>
2023-11-14 19:22:00 +0100

    Cleaned up and added a few customization options to the support script

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/bash -eux
+
+# Port on your local machine that you want esc-configurator to be listening on
+LOCAL_PORT=8234
+
+# Fetch tag of latest release version
+VERSION=$(curl --silent "https://api.github.com/repos/stylesuxx/esc-configurator/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 docker  buildx \
         build \
+        --build-arg VERSION=$VERSION \
         --progress plain \
         --platform linux/amd64 \
         -t esc-configurator:latest \
@@ -9,10 +16,27 @@ docker  buildx \
 
 docker  run \
         -i \
-        -d --publish 1234:1234 \
+        -d --publish $LOCAL_PORT:1234 \
         --privileged \
         -v /dev/bus/usb:/dev/bus/usb \
         esc-configurator:latest
 
-google-chrome-stable http://localhost:1234
+# This bit tries to guess the binary name of google chrome
+BIN1=google-chrome
+BIN2=google-chrome-stable
+BIN3=google-chrome-stable
 
+if (which $BIN1 &>/dev/null); then
+    $BIN1 http://localhost:$LOCAL_PORT
+elif (which $BIN2 &>/dev/null); then
+    $BIN2 http://localhost:$LOCAL_PORT
+elif (which $BIN3 &>/dev/null); then
+    $BIN3 http://localhost:$LOCAL_PORT
+else
+    echo
+    echo "The $BIN1 binary was not found on this machine."
+    echo "It is required to open esc-configurator."
+    echo "Exiting."
+    echo
+    exit 1
+fi
